7dc2dd1a66c869940d2c0d62135939e114fb09f7
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-28 21:28:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
@@ -14,4 +15,12 @@ def nyc_pigeon_organizer(data)
     end
   end
 end
+=======
+  hash.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+    binding.pry
+    final_array
+  end
+  binding.pry
+>>>>>>> be34836a281ddccf49d910d6379231c2a2c2c92f
 end
